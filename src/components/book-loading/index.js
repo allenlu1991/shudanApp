dd39ed61ff419ce95ed5962da1e9dcbac7bcf4be
allenlugit config --global user.email 903949263@qq.com <903949263@qq.com>
2019-05-17 00:16:19 +0800

    增加loading动画

--- src/components/book-loading/index.js
+++ src/components/book-loading/index.js
@@ -8,19 +8,21 @@ export default class BookLoading extends Component {
 
   render () {
     return (
-      <View className='book-loading'>
-        <View className="loader">
-          <View className="loader-inner"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
-          <View className="loader-inner box box-1"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
-          <View className="loader-inner box box-2"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
-          <View className="loader-inner box box-3"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
-          <View className="loader-inner box box-4"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
-          <View className="loader-inner box box-5"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
-          <View className="loader-inner box box-6"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
-          <View className="loader-inner"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
+      <View  className='book-loading-mask'>
+        <View className='book-loading'>
+          <View className="loader">
+            <View className="loader-inner"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
+            <View className="loader-inner box box-1"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
+            <View className="loader-inner box box-2"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
+            <View className="loader-inner box box-3"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
+            <View className="loader-inner box box-4"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
+            <View className="loader-inner box box-5"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
+            <View className="loader-inner box box-6"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
+            <View className="loader-inner"><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View><View className='book-loading-line'></View></View>
+          </View>
+          
+          <Text className='book-loading-text'>努力加载中</Text>
         </View>
-        
-        <Text className='book-loading-text'>努力加载中</Text>
       </View>
     )
   }
